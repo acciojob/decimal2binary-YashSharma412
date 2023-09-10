833cd1de7e1605736df6f02dd6686b39ad071a68
Yash Sharma <139811734+YashSharma412@users.noreply.github.com>
2023-09-10 20:08:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function decimalToBinary(num) {
   //Write you code here
-  
-}
+  let arr = [];
+	while (num!=0) {
+		let rem = num%2;
+		arr.unshift(rem);
+		num = parseInt(num/2);
+	}
+	return arr.join("");
 
+}
+// decimalToBinary(10);
 window.decimalToBinary = decimalToBinary;
